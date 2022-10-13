79208cca5f2293b082f45a6259303044cd357408
torako-san <gldemo1207@gmail.com>
2022-10-13 15:23:58 +0900

    Initial commit

---
+++ src/main/java/com/example/contacts/controllers/PersonController.java
@@ -0,0 +1,82 @@
+package com.example.contacts.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.contacts.models.Person;
+import com.example.contacts.repository.PersonRepository;
+
+import javax.annotation.PostConstruct;
+
+@Controller
+public class PersonController {
+
+  // Personクラスのフィールドをfinalにする。
+  private final PersonRepository repository;
+
+  public PersonController(PersonRepository repository) {
+    this.repository = repository;
+  }
+
+  @GetMapping("/")
+  public String index(@ModelAttribute Person person, Model model) {
+    // 一覧用データの用意
+    model.addAttribute("people", repository.findAll());
+    return "person/index";
+  }
+
+  @PostMapping("/create")
+  public String create(@Validated @ModelAttribute Person person, BindingResult result, Model model) {
+
+    // バリデーションエラーがある場合はindex.htmlを表示
+    if (result.hasErrors()) {
+      model.addAttribute("people", repository.findAll());
+      return "person/index";
+    }
+    repository.saveAndFlush(person);
+    return "redirect:/";
+  }
+
+  @GetMapping("/delete/{id}") // 初期データの投入
+  public String remove(@PathVariable long id) {
+    repository.deleteById(id);
+    return "redirect:/";
+  }
+
+  @GetMapping("/edit/{id}")
+  public String edit(@PathVariable long id, Model model) {
+    model.addAttribute("person", repository.findById(id)); // 一覧用データの用意
+    return "person/edit";
+  }
+
+  @PostMapping("/update/{id}")
+  public String update(@PathVariable long id, @Validated @ModelAttribute Person person, BindingResult result) {
+    if (result.hasErrors()) {
+      return "person/edit";
+    }
+    repository.save(person);
+    return "redirect:/";
+  }
+
+  // 初期データの投入
+  @PostConstruct
+  public void dataInit() {
+    Person suzuki = new Person();
+    suzuki.setName("鈴木");
+    suzuki.setAge(23);
+    suzuki.setEmail("suzuki@email.com");
+    repository.saveAndFlush(suzuki);
+
+    Person sato = new Person();
+    sato.setName("佐藤");
+    sato.setAge(20);
+    sato.setEmail("sato@email.com");
+    repository.saveAndFlush(sato);
+  }
+}
---
+++ src/main/java/com/example/contacts/models/Person.java
@@ -0,0 +1,38 @@
+package com.example.contacts.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+
+public class Person {
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  @NotBlank
+  @Size(max = 120)
+  private String name;
+
+  @NotNull
+  @Min(0)
+  @Max(120)
+  private Integer age;
+
+  @NotBlank
+  @Email
+  @Size(max = 254)
+  private String email;
+}
